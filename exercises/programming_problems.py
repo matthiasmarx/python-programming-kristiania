13a80c8ae15a4685ba86ec12dfe47f21bb10d0fe
Matthias Marx <matthias.marx@smail.th-koeln.de>
2024-01-17 10:29:21 +0100

    started with programming problems

---
+++ exercises/programming_problems.py
@@ -0,0 +1,32 @@
+import datetime
+import math
+from tabulate import tabulate
+
+
+def exercise1_KgToPound():
+    kg = float(input("Enter a weight in kg: "))
+    pound = kg * 2.2
+    print("The weight in pounds is: ", pound)
+
+
+def exercise2_calculateTheAge():
+    age = int(input("Enter your year of birth: "))
+    todaysYear = datetime.datetime.now().year
+
+    print("Your age is: ", todaysYear - age)
+
+
+def exercise3_calculateDataOfAcircle():
+    print("Please enter a radius of a circle: ")
+    radius = float(input())
+    print(tabulate([
+        ["Area", math.pi * radius ** 2],
+        ["Circumference", 2 * math.pi * radius],
+        ["Volume", 4/3 * math.pi * radius ** 3],
+        ["Surface", 4 * math.pi * radius ** 2]
+    ], ["Data", "Result"]))
+
+
+# exercise1_KgToPound()
+# exercise2_calculateTheAge()
+exercise3_calculateDataOfAcircle()
